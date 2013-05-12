8b30c8cec53c9e75b8705f1c0f379a3d7d7f5ad5
Seth Thompson <s3th.thompson@gmail.com>
2013-05-11 22:38:37 -0400

    form donegit stash pop

--- app/controllers/crushes_controller.rb
+++ app/controllers/crushes_controller.rb
@@ -7,6 +7,11 @@ class CrushesController < ApplicationController
 	def update
     @crush = current_user
     @crush.update_attributes(params[:crush])
+    params[:hint].keys.sort.each_with_index do |key, i|
+    	if params[:hint][key] then
+    		Hint.create(:email => params[:crush][("crush"+(i+1).to_s).to_sym], :content => params[:hint][key])
+    	end
+    end
    	redirect_to '/'
   end
 end
