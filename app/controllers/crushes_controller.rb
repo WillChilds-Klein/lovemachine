1c9c82e66ea985d1d8c781877f36b1b2ca6e75c5
Seth Thompson <s3th.thompson@gmail.com>
2013-05-13 11:04:40 -0400

    index page polishing + blank hint bug

--- app/controllers/crushes_controller.rb
+++ app/controllers/crushes_controller.rb
@@ -10,8 +10,9 @@ class CrushesController < ApplicationController
     @crush = current_user
     @crush.update_attributes(params[:crush])
     params[:hint].keys.sort.each_with_index do |key, i|
-    	if params[:hint][key] then
-    		Hint.create(:email => params[:crush][("crush"+(i+1).to_s).to_sym],
+        email = params[:crush][("crush"+(i+1).to_s).to_sym]
+    	unless params[:hint][key].blank? || email.blank? then
+    		Hint.create(:email => email,
                 :content => params[:hint][key],
                 :author => @crush.email)
     	end
