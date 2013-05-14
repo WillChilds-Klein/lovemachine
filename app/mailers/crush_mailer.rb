6881180387ce8371f47782b645fee2b21f5c2e76
Will Childs-Klein <willck93@gmail.com>
2013-05-14 08:32:27 -0400

    subject line contain crush info

--- app/mailers/crush_mailer.rb
+++ app/mailers/crush_mailer.rb
@@ -10,6 +10,6 @@ class CrushMailer < ActionMailer::Base
   	@crusher = crusher
   	@crushee = crushee
 
-  	mail(:to => "#{crusher.full_n} <#{crusher.email}>", :from => "Yale LoveMachine <lovemachine@herokuapp.com>", :subject => "You'll want to open this. Trust me.").deliver!
+  	mail(:to => "#{crusher.full_n} <#{crusher.email}>", :from => "Yale LoveMachine <lovemachine@herokuapp.com>", :subject => "You and " + @crushee.full_n + " have each crushed eachother on the Yale LoveMahine!").deliver!#:subject => "You'll want to open this. Trust me.").deliver!
   end
 end
