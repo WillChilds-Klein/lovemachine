13821a34fabf9b3ef49dacc06eb1c2b72bcdd32d
Will Childs-Klein <willck93@gmail.com>
2013-05-14 01:37:11 -0400

    hmmmm

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,11 +40,11 @@ Lovemachine::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'baci.lindsaar.net',
-    :user_name            => 'yaleseniorcupid',
-    :password             => 'lovemachine',
-    :authentication       => 'plain',
+    :address              => "smtp.sendgrid.net",
+    :port                 => '587',
+    :domain               => 'heroku.com',
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :authentication       => :plain,
     :enable_starttls_auto => true  }
 end
